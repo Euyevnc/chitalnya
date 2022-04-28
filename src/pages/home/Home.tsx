3acbc2653e6067929142e309836c9f3db4e6534a
Euyevnc <remarka234@gmail.com>
2022-04-28 17:27:57 +0700

    init

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+  return (
+    <div className="home">
+      <div className="home__preview"></div>
+    </div>
+  );
+};
+
+export default Home;
